c673d1c573091011dbc17e6b447e58510b51dfba
Jared Grippe <jared@deadlyicon.com>
2016-10-14 21:42:36 -0700

    WIP

--- browser/index.js
+++ browser/index.js
@@ -1,1 +1,9 @@
-console.log('browser.js')
+import React from 'react'
+import { render } from 'react-dom'
+import './index.sass'
+import Root from './components/Root';
+
+render(
+  <Root />,
+  document.querySelector('main')
+);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('server.js')
+import path from 'path'
+import express from 'express'
+
+const publicPath = path.resolve(__dirname, '../public')
+
+const server = express()
+server.set('env', process.env.NODE_ENV)
+server.set('port', process.env.PORT || '3000')
+
+server.use(express.static(publicPath))
+
+server.get('/*', (request, response) => {
+  response.sendFile(publicPath+'/index.html')
+});
+
+if (process.env.NODE_ENV !== 'test'){
+  server.listen(server.get('port'))
+}
