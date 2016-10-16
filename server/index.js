07885465122e9e337ff4cbe71ee446359629ca19
Jared Grippe <jared@deadlyicon.com>
2016-10-16 15:20:56 -0700

    WIP

--- server/index.js
+++ server/index.js
@@ -9,10 +9,17 @@ server.set('port', process.env.PORT || '3000')
 
 server.use(express.static(publicPath))
 
+server.get('/hello-world', (request, response) => {
+  response.send('Hello World')
+});
+
 server.get('/*', (request, response) => {
   response.sendFile(publicPath+'/index.html')
 });
 
 if (process.env.NODE_ENV !== 'test'){
+  console.log('http://localhost:'+server.get('port')+'/')
   server.listen(server.get('port'))
 }
+
+export default server
